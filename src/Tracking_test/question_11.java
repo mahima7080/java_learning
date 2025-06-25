d6c19eba9ea08af9bb9f5096214a638f076d834f
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-25 09:30:08 +0530

    11 n 12

---
+++ src/Tracking_test/question_11.java
@@ -0,0 +1,17 @@
+package Tracking_test;
+
+import java.util.Scanner;
+
+public class question_11 {
+    public static void main(String[] args) {
+        System.out.println("enter the value of n" );
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+
+        for (int i=1;i<=n;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Tracking_test/question_12.java
@@ -0,0 +1,24 @@
+package Tracking_test;
+
+import java.util.Scanner;
+
+public class question_12 {
+    public static void main(String[] args) {
+        int num,fact=1;
+        System.out.println("enter number");
+        Scanner sc=new Scanner(System.in);
+        num=sc.nextInt();
+        if (num==0)
+        {
+            System.out.println("1");
+            return;
+        }
+
+        while(num>0)
+        {
+            fact=fact* num;
+            num--;
+        }
+        System.out.println(fact);
+    }
+}
