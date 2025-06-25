c064eb5ab04ea78fc9e1a07ba0e2232df7d0582a
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-25 08:40:01 +0530

    q1

---
+++ src/Tracking_test/question_2.java
@@ -0,0 +1,12 @@
+package Tracking_test;
+
+public class question_2 {
+    public static void main(String[] args) {
+        int a=10, b=5;
+        System.out.println("Addition: "+(a+b));
+        System.out.println("Subtraction:" + (a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("division: "+a/b);
+        System.out.println("Modulo: "+a%b);
+    }
+}
