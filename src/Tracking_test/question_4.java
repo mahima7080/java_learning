89db4ab65c3800d5438ffdc686a453ef05505059
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-25 08:53:50 +0530

    q3 and q4

---
+++ src/Tracking_test/question_4.java
@@ -0,0 +1,16 @@
+package Tracking_test;
+
+public class question_4 {
+    public static void main(String[] args) {
+        int a=5;
+        System.out.println(" original "+ a);
+        System.out.println(" pre increment "+ ++a);
+        System.out.println(" post increment "+ a++);
+        System.out.println(" pre decrement "+ --a);
+        System.out.println(" post decrement "+ a--);
+
+
+
+
+    }
+}
