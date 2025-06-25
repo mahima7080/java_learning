40a5a0576ed760ff05780e16b9cf3ce4fceb2d78
mahima7080 <mahimasrivastava84@gmail.com>
2025-06-25 09:04:57 +0530

    all 5 and 6

--- src/Tracking_test/question_5.java
+++ src/Tracking_test/question_5.java
@@ -1,4 +1,10 @@
 package Tracking_test;
 
 public class question_5 {
+    public static void main(String[] args) {
+        int a=10;
+        double d=3.5;
+        System.out.println(a+d);
+        System.out.println((int)(a+d));
+    }
 }
--- src/Tracking_test/question_6.java
+++ src/Tracking_test/question_6.java
@@ -1,4 +1,23 @@
 package Tracking_test;
 
+import java.util.Scanner;
+
 public class question_6 {
+    public static void main(String[] args) {
+        int num;
+        System.out.println("enter any no");
+        Scanner sc=new Scanner(System.in);
+        num=sc.nextInt();
+        if(num>0)
+        {
+            System.out.println("postive");
+        }
+        else if (num<0)
+        {
+            System.out.println("negative");
+        }
+        else {
+            System.out.println("zero");
+        }
+    }
 }
